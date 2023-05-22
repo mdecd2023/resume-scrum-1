7aeb285d1684d93c79cc84cf4fb20c9599ca7b44
mdecycu <mdecycuorg@gmail.com>
2023-05-22 06:59:08 +0000

    add solid edge simulation result

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from waitress import serve
 from cmsimde import flaskapp
-
-serve(flaskapp.app, listen='127.0.0.1:9500', threads=8)
+# 8xxxx is for Stunnel accept port
+# 9xxxx is for localhost internal port
+serve(flaskapp.app, listen='127.0.0.1:9001', threads=8)
